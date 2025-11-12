faa405a01c8bdfc38a7987d38f7682fafa1f2f33
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2025-11-12 22:39:39 +0100

    Fix Netlify build error - remove optimizeCss

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     domains: ['i.ytimg.com', 'img.youtube.com'], // YouTube thumbnails
     formats: ['image/avif', 'image/webp'],
   },
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 module.exports = nextConfig
